9673e225c333e928a79724bc88628c814f581b71
Fernando Albornoz <fernando.ezequiel.albornoz@gmail.com>
2023-03-07 14:04:58 -0300

    modified

--- client/src/components/MovieDetail/MovieDetail.jsx
+++ client/src/components/MovieDetail/MovieDetail.jsx
@@ -30,7 +30,9 @@ const MovieDetail = () => {
     <>
       <div className="containerGlobalDetail">
         <div className="detailCard">
+          <div className="containerImageDetail">
             <img className="imageDetail" src={movieDetail.image} alt="imagen" />
+            </div>
           <div className="overviewDetail">
             <div className="containerHeadDetail">
               <h1 className="titleDetail">{movieDetail.title}</h1>
